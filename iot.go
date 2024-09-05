bea294c2620462148b4f542c912465beb1672398
yashweblife <yashweblife20@gmail.com>
2024-09-05 16:47:48 -0500

    add new function

--- iot.go
+++ iot.go
@@ -2,6 +2,8 @@ package iot
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -46,7 +48,51 @@ func (d *Device) TriggerGetInfoFromDevice() (DeviceInfo, error) {
 	if err != nil {
 		return DeviceInfo{}, err
 	}
+	d.Url = data.ip
+	d.Commands = data.Commands
 	return data, nil
+}
+func (d *Device) TriggerCommand(name string) error {
+	if d.Url == "" {
+		return errors.New("url is empty")
+	}
+	if len(d.Commands) == 0 {
+		return errors.New("no commands")
+	}
+	check := false
+	for _, v := range d.Commands {
+		if v.Name == name {
+			check = true
+		}
+	}
+	if !check {
+		return errors.New("command not found")
+	}
+
+	client := http.DefaultClient
+	req, err := http.NewRequest("POST", d.Url+name, nil)
+	if err != nil {
+		return err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return err
+	}
+	var data string
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return err
+	}
+	fmt.Println(data)
+	return nil
 }
 
 type IOT struct {
